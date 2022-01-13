d74d583bd776971197620650d2ef6a7c8aeb3c4c
BORBO <bwofodol@gmail.com>
2022-01-13 03:54:22 -0700

    Moved Ascended Vortex to BORBO. New logs courtesy of orange

--- DuckItemPack/Items/BigBattery.cs
+++ DuckItemPack/Items/BigBattery.cs
@@ -32,11 +32,67 @@ namespace DuckItemPack.Items
             $"by <style=cIsDamage>{Tools.ConvertDecimal(rechargeRateIncrease)}</style> " +
             $"<style=cStack>(+{Tools.ConvertDecimal(rechargeRateIncrease)} per stack)</style>.";
 
+        /*public override string ItemLore =>
+            "Okay, how’s this one?" +
+            "\n\nWe’ve uh," +
+            "\n\n*snicker*" +
+            "\n\nUh, we’ve reviewed your notes on the last prototype, and, uh" +
+            "\n\n(background chatter)" +
+            "\n\nWe’ve adjusted the dimensions slightly" +
+            "\n\n*snicker*" +
+            "\n\nNow, it is slightly longer than your generator thing, but, uh" +
+            "\n\n(muffled laughter)" +
+            "\n\nWe also drew up this little, uh, connector thing…" +
+            "\n\n(Shut the fuck up guys)" +
+            "\n\nUh, anyway, you can just, like, stick it right in there, like in the demo video" +
+            "\n\n(loud footsteps)" +
+            "\n\nAnd honestly I think we’re," +
+            "\n\n*snicker*" +
+            "\n\nI think we’re really onto something here, man" +
+            "\n\nLike this could be the uh" +
+            "\n\nThe new standard power supply" +
+            "\n\n*snicker*" +
+            "\n\nYou know?";*/
         public override string ItemLore => 
-            "A scientist walked out in a rage, shouting 'Screw naming conventions!' " +
-            "and placed what appeared to be a standard battery, shrunk, " +
-            "with comically long dimensions onto the table. " +
-            "\n \nIt is currently in storage while the scientist is under review.";
+@"Okay, how’s this one?
+
+We’ve uh,
+
+*snicker*
+
+Uh, we’ve reviewed your notes on the last prototype, and, uh
+
+(background chatter)
+
+We’ve adjusted the dimensions slightly
+
+*snicker*
+
+Now, it is slightly longer than your generator thing, but, uh
+
+(muffled laughter)
+
+We also drew up this little, uh, connector thing…
+
+(Shut the fuck up guys)
+
+Uh, anyway, you can just, like, stick it right in there, like in the demo video
+
+(loud footsteps)
+
+And honestly I think we’re,
+
+*snicker*
+
+I think we’re really onto something here, man
+
+Like this could be the uh
+
+The new standard power supply
+
+*snicker*
+
+You know?";
 
         public override ItemTier Tier => ItemTier.Tier2;
         public override ItemTag[] ItemTags { get; set; } = new ItemTag[] { ItemTag.Utility , ItemTag.Damage };
